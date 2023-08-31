e95fdd1cc25420746d519d6dd65cb77c0d9f37b0
Erik Lindebratt <erik.lindebratt@gmail.com>
2023-08-31 21:08:24 +0200

    chore: Add WezTerm configs

---
+++ .wezterm.lua
@@ -0,0 +1,30 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+config.font_size = 16
+config.font = wezterm.font_with_fallback({
+	"Menlo",
+	"Hack Nerd Font Mono",
+})
+config.color_scheme = "tokyonight"
+config.check_for_updates = true
+config.keys = {
+	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+	-- Make Option-Right equivalent to Alt-f; forward-word
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+}
+
+-- and finally, return the configuration to wezterm
+return config
