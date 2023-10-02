aa7e1af8149c9de7e5b7353d89211f3ac4bed7f1
Erik Lindebratt <erik.lindebratt@gmail.com>
2023-10-02 23:08:06 +0200

    fix(vim): Auto-close neotree when a file is opened

--- nvim/lua/plugins/filetree.lua
+++ nvim/lua/plugins/filetree.lua
@@ -13,6 +13,14 @@ return {
         -- instead of relying on nvim autocmd events.
         use_libuv_file_watcher = true,
       },
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function()
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
     },
   },
 }
