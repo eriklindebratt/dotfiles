0fb5cb0538850c778bd415c8f6376d35ac467c8b
Erik Lindebratt <erik.lindebratt@gmail.com>
2023-10-04 11:00:00 +0200

    fix: Disable bufferline

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
   -- disable `flash.nvim` to e.g. keep default behavior of `s` and `f` key(s)
   { "folke/flash.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
 }
