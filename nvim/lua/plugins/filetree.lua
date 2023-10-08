e888098cdea4367377e7c3b63bbd6b6b9d345644
Erik Lindebratt <erik.lindebratt@gmail.com>
2023-10-08 23:35:22 +0200

    fix(vim): Use `o` in addition to `<CR>` to open a file in filetree

--- nvim/lua/plugins/filetree.lua
+++ nvim/lua/plugins/filetree.lua
@@ -3,6 +3,14 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     -- opts will be merged with the parent spec
     opts = {
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function()
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
       filesystem = {
         filtered_items = {
           visible = true,
@@ -12,13 +20,18 @@ return {
         -- This will use the OS level file watchers to detect changes
         -- instead of relying on nvim autocmd events.
         use_libuv_file_watcher = true,
+
+        window = {
+          mappings = {
+            ["o"] = "open",
+          },
+        },
       },
-      event_handlers = {
-        {
-          event = "file_opened",
-          handler = function()
-            require("neo-tree.command").execute({ action = "close" })
-          end,
+      buffers = {
+        window = {
+          mappings = {
+            ["o"] = "open",
+          },
         },
       },
     },
