51d8b738fcea25a257c11d958f737939777ccf6f
Erik Lindebratt <erik.lindebratt@gmail.com>
2023-10-10 14:16:45 +0200

    fix(vim): Remove superfluous keybinding
    Search "move" in `:Telescope keymaps` for existing keybinding from
    LazyVim.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -17,12 +17,4 @@ vim.cmd([[
   cnoreabbrev Noh noh
 ]])
 
--- Move selected lines up or down by one line:
---   1. Move one line up/down
---   2. Re-select selection
---   3. Indent
---   4. Re-select selection
-vim.keymap.set("v", "<C-k>", ":move '<-2<CR>gv=gv")
-vim.keymap.set("v", "<C-j>", ":move '>+1<CR>gv=gv")
-
 vim.keymap.set("n", "<leader>/", ":noh<CR>") -- remove search highlights
