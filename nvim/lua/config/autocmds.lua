b64ef4bdd11453b069036b4057969b7a00114ed4
Erik Lindebratt <erik.lindebratt@gmail.com>
2023-10-25 20:57:48 +0200

    fix(vim): Close Neotree when file is opened from e.g. Telescope

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- When existing files are opened in another way than from Neotree (e.g. using Telescope), close Neotree
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+  callback = function()
+    vim.cmd("Neotree close")
+  end,
+})
