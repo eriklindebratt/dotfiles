83581fd0c2b7d888fde0e11d5fc0539fd127c632
Erik Lindebratt <erik.lindebratt@gmail.com>
2024-01-30 19:16:35 +0100

    fix(vim): Add custom command to reveal current file in Finder

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -21,3 +21,9 @@ vim.keymap.del("n", "<c-_>") -- remove binding to open floating terminal
 vim.keymap.del("n", "<c-/>") -- remove binding to open floating terminal
 
 vim.keymap.set("n", "<leader>/", ":noh<CR>") -- remove search highlights
+
+-- reveal current file in Finder
+vim.api.nvim_create_user_command("Rfinder", function()
+  local path = vim.api.nvim_buf_get_name(0)
+  os.execute("open -R " .. path)
+end, {})
