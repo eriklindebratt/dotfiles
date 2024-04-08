15af05ef35f97740a00a5dcc827382b854fd1405
Erik Lindebratt <erik.lindebratt@gmail.com>
2024-04-08 22:09:51 +0200

    fix(vim): Hide current branch and clock from statusline

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -28,7 +28,7 @@ return {
       },
       sections = {
         lualine_a = { "mode" },
-        lualine_b = { "branch" },
+        lualine_b = {},
 
         lualine_c = {
           LazyVim.lualine.root_dir(),
@@ -100,11 +100,7 @@ return {
           { "progress", separator = " ", padding = { left = 1, right = 0 } },
           { "location", padding = { left = 0, right = 1 } },
         },
-        lualine_z = {
-          function()
-            return " " .. os.date("%R")
-          end,
-        },
+        lualine_z = {},
       },
       extensions = { "neo-tree", "lazy" },
     }
