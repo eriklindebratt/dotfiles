c740ef1c2890cdaf100d14297a0ab6aa66f24fc5
Erik Lindebratt <erik.lindebratt@gmail.com>
2024-05-12 20:38:11 +0200

    chore(vim): Install `css-variables-language-server` as part of bootstrap

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -10,6 +10,7 @@ return {
         "bash-language-server",
         "black",
         "css-lsp",
+        "css-variables-language-server",
         "deno",
         "dockerfile-language-server",
         "eslint-lsp",
