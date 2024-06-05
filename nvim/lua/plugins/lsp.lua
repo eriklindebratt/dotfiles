92a9297d8d302c73d42000c6398b23292dc0614d
Erik Lindebratt <erik.lindebratt@gmail.com>
2024-06-05 23:36:28 +0200

    fix

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,10 +1,9 @@
+---@return string[]
 local function get_abs_deno_enable_paths()
   ---@type string[]
-  local deno_enable_paths = require("neoconf").get("vscode.deno.enablePaths")
-
-  if not deno_enable_paths then
-    return {}
-  end
+  local deno_enable_paths = {}
+  vim.list_extend(deno_enable_paths, require("neoconf").get("vscode.deno.enablePaths") or {})
+  vim.list_extend(deno_enable_paths, (require("neoconf").get("lspconfig.denols") or {})["deno.enablePaths"] or {})
 
   local unique_deno_enable_paths = {}
   for _, unexpanded_path in ipairs(deno_enable_paths) do
@@ -15,18 +14,15 @@ local function get_abs_deno_enable_paths()
       unique_deno_enable_paths[vim.fn.fnamemodify(path, ":p")] = true
     end
   end
+
   return vim.tbl_keys(unique_deno_enable_paths)
 end
 
 ---@param filename string
 local function is_deno_enabled_file(filename)
   local file_path = vim.fn.fnamemodify(filename, ":p")
-  -- print("deno enable paths absolute: " .. vim.inspect(get_abs_deno_enable_paths()))
   for _, path in ipairs(get_abs_deno_enable_paths()) do
     local match = string.find(file_path, path, 1, true)
-    -- print("file_path: " .. vim.inspect(file_path))
-    -- print("path: " .. vim.inspect(path))
-    -- print("match: " .. vim.inspect(match))
 
     if match then
       return true
@@ -36,28 +32,64 @@ local function is_deno_enabled_file(filename)
   return false
 end
 
--- return {}
-
--- lspconfig
 return {
-  "neovim/nvim-lspconfig",
-  ---@class PluginLspOpts
-  opts = function(_, opts)
-    return vim.tbl_deep_extend("force", opts, {
-      inlay_hints = {
-        enabled = false,
-      },
-      setup = {
-        vtsls = function(_, opts)
-          opts.single_file_support = false
-          opts.root_dir = function(filename, bufnr)
-            if is_deno_enabled_file(filename) then
-              return nil
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      return vim.tbl_deep_extend("force", opts, {
+        inlay_hints = {
+          enabled = false,
+        },
+        setup = {
+          vtsls = function(_, opts)
+            opts.single_file_support = false -- See comment below
+            opts.root_dir = function(filename)
+              if is_deno_enabled_file(filename) then
+                -- Setting `root_dir` to `nil `will prevent the LSP client from attaching to a buffer - but only if `single_file_support = false`
+                return nil
+              end
+              return require("lspconfig.util").root_pattern("package.json")(filename)
             end
-            return require("lspconfig.util").root_pattern("package.json")(filename)
-          end
-        end,
-      },
-    })
-  end,
+          end,
+        },
+      })
+    end,
+  },
+
+  {
+    {
+      "williamboman/mason.nvim",
+
+      opts = function(_, opts)
+        local ensure_installed = {
+          -- NOTE: Remember to add languages in `treesitter.lua` as necesssary
+          "bash-language-server",
+          "black",
+          "css-lsp",
+          "css-variables-language-server",
+          "deno",
+          "dockerfile-language-server",
+          "eslint-lsp",
+          "gopls",
+          "graphql-language-service-cli",
+          "html-lsp",
+          "json-lsp",
+          "lua-language-server",
+          "nginx-language-server",
+          "prettier",
+          "pyright",
+          "tailwindcss-language-server",
+          "tflint",
+          "vtsls",
+          "shellcheck",
+          "stylelint",
+          "svelte-language-server",
+          "sqlls",
+          -- see lazy.lua for LazyVim extras
+        }
+
+        vim.list_extend(opts.ensure_installed, ensure_installed or {})
+      end,
+    },
+  },
 }
