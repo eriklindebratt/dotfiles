3b4be9bdc8c8aaefb5f762c53fcb644f8a855c65
Erik Lindebratt <erik.lindebratt@gmail.com>
2025-03-30 23:37:54 +0200

    chore: Fix deprecation warning

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -13,10 +13,10 @@ return {
     config = {
       update_interval = 1000,
       set_dark_mode = function()
-        vim.api.nvim_set_option("background", "dark")
+        vim.api.nvim_set_option_value("background", "dark", {})
       end,
       set_light_mode = function()
-        vim.api.nvim_set_option("background", "light")
+        vim.api.nvim_set_option_value("background", "light", {})
       end,
     },
   },
