818d6f80053873cb668e99bbabbfb5744605d74a
Erik Lindebratt <erik.lindebratt@gmail.com>
2025-05-15 08:57:58 +0200

    fix(vim): Zen file explorer and picker keybinding

--- nvim/lua/plugins/filetree.lua
+++ nvim/lua/plugins/filetree.lua
@@ -5,6 +5,23 @@ return {
       explorer = {
         replace_netrw = false,
       },
+      picker = {
+        sources = {
+          files = {
+            hidden = true,
+          },
+          explorer = {
+            hidden = true, -- show hidden files
+            win = {
+              list = {
+                keys = {
+                  ["<c-t>"] = { "edit_tab", mode = { "i", "n" } }, -- default keybinding is to open terminal, remap to open in new tab
+                },
+              },
+            },
+          },
+        },
+      },
     },
   },
 }
