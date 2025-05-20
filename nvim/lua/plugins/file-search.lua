c72b6f4a9fda70a8a9635561d096ad3a867e9a7a
Erik Lindebratt <erik.lindebratt@gmail.com>
2025-05-20 10:08:40 +0200

    fix(vim): Include hidden files when grepping for string across files

---
+++ nvim/lua/plugins/file-search.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          grep = {
+            hidden = true, -- show hidden files
+          },
+        },
+      },
+    },
+  },
+}
